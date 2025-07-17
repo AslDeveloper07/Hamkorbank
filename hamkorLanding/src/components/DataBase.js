e3efbe15d5d2ec331d5dc7cfdbc94ea31b14e9ed
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-17 18:45:33 +0500

    first commit

---
+++ hamkorLanding/src/components/DataBase.js
@@ -0,0 +1,98 @@
+const NavbarData = {
+  logo: "https://hamkorbank.uz/assets/images/static/logo.svg",
+  links: [
+    "Kreditlar",
+    "Kartalar",
+    "Ipoteka",
+    "Omonatlar",
+    "Valyutalar",
+    "kursi",
+    "Yana",
+  ],
+};
+
+
+const CardData = [
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/loans_new_xl.196cae40.webp",
+    title: "Kreditlar",
+    poragraph: "Onlayn rasmiylashtiriladi",
+    btnTitle: 'Rasmiylashtirish'
+  },
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/debit_cards_xl.196cae40.webp",
+    title: "Debet kartalar",
+    poragraph: "Yutuqli aksiyalar",
+    btnTitle: 'Ochish'
+  },
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/credit_card_xl.196cae40.webp",
+    title: "Kredit karta",
+    poragraph: "Odatdagidan tezroq",
+    btnTitle: 'Rasmiylashtirish'
+  },
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/mortgage_xl.196cae40.webp",
+    title: "Ipoteka",
+    poragraph: "20 yilgacha",
+    btnTitle: 'Rasmiylashtirish'
+  },
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/deposits_xl.196cae40.webp",
+    title: "Omonatlar",
+    poragraph: "Har oyda daromad",
+    btnTitle: 'Rasmiylashtirish'
+  },
+  {
+    img: "https://hamkorbank.uz/assets/images-cache/products/physical/home-slider-menu/currency_exchange_xl.196cae40.webp",
+    title: "Valyuta ayirboshlash",
+    poragraph: "Foydali kurs",
+    btnTitle: 'Ayirboshlash'
+  },
+];
+
+
+const GridData = [
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/oniline_microloan_xl.9fdbcb93.webP',
+    gridTitle: 'Onlayn kredit',
+    gridPoragraph: 'Pulni 1 daqiqada oling',
+    gridBtn: 'Rasmiylashtirish'
+
+  },
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/visa_classic_xl.d989851b.webP',
+    gridTitle: 'Visa debet kartasi',
+    gridPoragraph: 'Butun dunyo bo‘ylab xarid qilish uchun',
+    gridBtn: <IoIosArrowForward />
+
+  },
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/mortgage_xl.d989851b.webP',
+    gridTitle: 'Ipoteka krediti',
+    gridPoragraph: 'Uy-joy ta’miri uchun',
+    gridBtn: <IoIosArrowForward />
+
+  },
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/credit_card_xl.d989851b.webP',
+    gridTitle: 'Kredit karta',
+    gridPoragraph: '1 daqiqada ma’qullaymiz, bepul rasmiylashtiramiz',
+    gridBtn: <IoIosArrowForward />
+
+  },
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/deposit_new_xl.d989851b.webP',
+    gridTitle: 'Oson 20 omonati',
+    gridPoragraph: 'Milliy valyutada, foizlar har oyda olinadi',
+    gridBtn: <IoIosArrowForward />
+
+  },
+  {
+    image: 'https://hamkorbank.uz/assets/images-cache/products/physical/popular-products/autoloan_xl.13c74c57.webP',
+    gridTitle: 'Avtokredit',
+    gridPoragraph: 'Yangi va minilgan avtomobillar xaridi uchun ',
+    gridBtn: 'Rasmiylashtirish'
+
+  },
+]
