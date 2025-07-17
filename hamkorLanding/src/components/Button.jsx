b02ec7abf65ed94833294f7dbe72c708f21b0f6b
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-17 21:45:54 +0500

    add

---
+++ hamkorLanding/src/components/Button.jsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+  return (
+    <button className="navBtn">
+      internet-bank
+    </button>
+  )
+}
+
+export default Button
--- hamkorLanding/src/components/Navbar.jsx
+++ hamkorLanding/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavbarData } from './DataBase';
 import './../style/index.css'
+import Button from './Button';
 
 const Navbar = () => {
   return (
     <header className='header'>
       <nav className='container'>
-     <div>
+     <div className='navLink'>
        <img
         src={NavbarData.logo}
         alt="Hamkorbank Logo"
@@ -22,6 +23,21 @@ const Navbar = () => {
         }
       </ul>
      </div>
+    <div className='nav-price'>
+       <div className='bank-price'>
+      <div className='price'>
+        <div className='price1'>
+         <p> Sotib olish</p>
+          <h5>12.600</h5>
+        </div>
+        <div className='price1'>
+         <p> Sotib olish</p>
+          <h5>12.600</h5>
+        </div>
+      </div>
+     </div>
+      <Button/>
+    </div>
     </nav>
     </header>
   );
